f65394ca853152c7126b9ba2a6acba01d388da86
Geoffrey Lovelace <geoffrey4444@gmail.com>
2022-05-17 07:57:01 -0700

    Initial Commit

---
+++ Shared/PiDartAsynApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiDartAsynApp.swift
+//  Shared
+//
+//  Created by Geoffrey Lovelace on 5/17/22.
+//
+
+import SwiftUI
+
+@main
+struct PiDartAsynApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
